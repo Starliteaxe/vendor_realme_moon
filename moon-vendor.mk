d06560bd18f7d213db18841c33a379d591c22ba6
DPSLEGEND <iqbal.singh.02.pb@gmail.com>
2022-02-12 13:03:08 +0530

    moon: Stop building Engineer mode

--- moon-vendor.mk
+++ moon-vendor.mk
@@ -36,6 +36,3 @@ PRODUCT_COPY_FILES += \
     vendor/realme/moon/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
     vendor/realme/moon/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
     vendor/realme/moon/proprietary/lib64/libem_support_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libem_support_jni.so \
-
-PRODUCT_PACKAGES += \
-    EngineerMode
